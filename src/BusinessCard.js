26250f9b6267530b56f37c41b126ef300979554a
Everson <41695343+eversonapp@users.noreply.github.com>
2020-12-29 16:51:32 -0300

    redoing justbusiness with a cleaner code and new api

--- src/BusinessCard.js
+++ src/BusinessCard.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 
-export default class BusinessSearch extends Component {
+export default class BusinessCard extends Component {
     constructor(props) {
         super(props);
         
         this.state = {
-            companyCod: "MSFT",
-            company: false,
+            company: [],
+            companyNews: [],
         }
     }
     
-    loadingCompany = async (companyCod) => {
-        const urlApi = "https://api.polygon.io/v1/meta/symbols/" +
-        companyCod +
-        "/company?apiKey=CS4QQvffYLSCRPZG6wGVK4A4xjEzGj_P" 
+    
+    loadingBusinessCard = async (companyCod) => {
+        const urlApi = 'https://financialmodelingprep.com/api/v3/profile/' +
+        companyCod +'?apikey=040969b1255adee69233f0ae39fcbdfe'
         fetch(urlApi)
             .then(res => res.json())
             .then(json =>{
@@ -23,63 +23,95 @@ export default class BusinessSearch extends Component {
             })
     }
 
-    handlerChange = (e) =>{
+    loadingBusinessNews = async (companyCod) => {
+        const urlApi = 'https://financialmodelingprep.com/api/v3/stock_news?tickers=' +
+        companyCod + '&limit=03&apikey=040969b1255adee69233f0ae39fcbdfe' 
+        fetch(urlApi)
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    companyNews: json
+                })
+            })
+    }
+
+    changeHandler = (e) => {
         const companyCod = e.target.value
         this.setState({companyCod})
 
-        this.loadingCompany(companyCod)
-
+        this.loadingBusinessCard(companyCod)
+        this.loadingBusinessNews(companyCod)
     }
 
     componentDidMount(){
-        this.loadingCompany(this.state.companyCod)
+        this.loadingBusinessCard()
+        this.loadingBusinessNews()
     }
 
     render() {
-        if(!this.loadingCompany){return alert("Max req")}
+        const {company, companyNews} = this.state
 
         return (
             <div>
-                <select className="btnSearch" value={this.setState.companyCod} onChange={this.handlerChange}>
-                    <option selected>Select the company</option>
-                    <option value="MSFT">Microsoft</option>
-                    <option value="AAPL">Apple</option>
-                    <option value="TSLA">Tesla</option>
-                </select>
-
-                <div className="businessProfile">
-                    <img src={this.state.company.logo} alt="Logo"></img>
-                    <ul>
-                        <li>
-                            <h1>
-                                {this.state.company.symbol}
-                                {this.state.company.name}
-                            </h1>
-                        </li>
-                        <li>
-                            Market Cap: {this.state.company.marketcap}
-                        </li>
-                        <li>
-                            IPO {this.state.company.listdate}
-                            Exchange: {this.state.company.exchange}
-                        </li>
-                        <li>
-                            Sector: {this.state.company.sector}
-                            Industry: {this.state.company.industry}
-                        </li>
-                        <li>
-                            Employees: {this.state.company.employees}
-                            CEO: {this.state.company.ceo}
-                        </li>
-                        <li>
-                            Headquarters: {this.state.company.hq_address}
-                            Country: {this.state.company.country}
-                        </li>
-                        <li>
-                            {this.state.company.description}
-                        </li>
-                    </ul>
+                <div>
+                    <select className="btnSearch" value={this.state.companyCod} onChange={this.changeHandler}>
+                        <option selected>Select the company</option>
+                        <option value="MSFT">Microsoft</option>
+                        <option value="AAPL">Apple</option>
+                        <option value="TSLA">Tesla</option>
+                    </select>
                 </div>
+
+                {company.map(item => (
+                <div key={item.id} className="businessCard">
+                    <div className="businessProfile">
+                        <img src={item.image} />
+                        <ul>
+                            <li>
+                                <span className="tags">Website </span> {item.website}
+                            </li>
+                            <li>
+                                <span className="tags">Market Cap: </span> {item.mktCap}
+                            </li>
+                            <li>
+                                <span className="tags">Exchange: </span> {item.exchangeShortName} <span className="tags">IPO: </span> {item.ipoDate}
+                            </li>
+                            <li>
+                                <span className="tags">Industry: </span> {item.industry} <span className="tags">Sector: </span> {item.sector}
+                            </li>
+                            <li>
+                            <span className="tags">CEO: </span> {item.ceo} <span className="tags">Employees: </span> {item.fullTimeEmployees}
+                            </li>
+                            <li>
+                                <span className="tags">Headquarters: </span> {item.city} - {item.state} - {item.country}
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="businessDesc">
+                        <h1>
+                            {item.companyName}
+                        </h1>
+                        <h2>
+                            Symbol: {item.symbol} price: {item.price}
+                        </h2>
+                        <p>
+                            {item.description}
+                        </p>
+                    </div>
+                </div>
+                ))}
+
+                {companyNews.map(item =>(
+                    <div key={item.id} className="businessNews">
+                        <img src={item.image} alt={item.symbol} title={item.symbol} />
+                        <div>
+                            <h3> {item.title} </h3>
+                            <h6> {item.site} </h6>
+                            <p> {item.text} </p>
+                            <a href={item.url}> Link </a>
+                        </div>
+                    </div>
+                ))}
             </div>
         );
     }
