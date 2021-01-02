b7df44763d38d19ff182b59ee24e36b135327b2e
Everson <41695343+eversonapp@users.noreply.github.com>
2021-01-02 20:49:01 -0300

    Logo tweaks and Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,13 @@
+import gitHub from './pics/github.png'
+
 export default function Footer() {
     return(
-        <footer>
-            Credits
+        <footer className="footer">
+            <div className="footerContainer">
+                <a href='https://github.com/eversonapp/businesspedia' target="_blank" rel='noreferrer'>
+                    <img src={gitHub} title='GitHub' alt='GitHub' />
+                </a>
+            </div>
         </footer>
     )
 }
