7b4aa1415d00a7007d03188119c0a1ed9bc98123
Everson <41695343+eversonapp@users.noreply.github.com>
2021-01-12 22:19:27 -0300

    About pg

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,13 @@ export default function About() {
             <div className="aboutContent">
                 <h1>About</h1>
                 <p>
+                    Cupidatat amet ipsum labore ut nostrud laboris ea. Deserunt ipsum elit elit esse aute elit non commodo ex Lorem. Excepteur consectetur enim aliquip id nulla. Est nisi qui ea laborum anim. Laborum mollit ut eu duis labore ea magna ex labore consequat id elit.
+                </p>
+                <h3>Goals</h3>
+                <p>
                     Amet deserunt consequat dolore culpa adipisicing Lorem ipsum laborum proident. Aute ut eiusmod esse pariatur ea. Adipisicing occaecat excepteur amet est quis velit fugiat consectetur sit elit. Ad consectetur irure id in incididunt. Consequat ea velit consectetur cillum esse eiusmod dolor proident dolor ea nulla laborum. Occaecat anim dolore pariatur ex voluptate duis. Anim est duis consequat adipisicing mollit incididunt cupidatat pariatur irure consequat.
                 </p>
+                <h3>Values</h3>
                 <p>
                     Ipsum eiusmod voluptate consectetur nulla ad magna. Sunt proident enim cupidatat qui nulla ipsum. Occaecat irure occaecat ea est reprehenderit aliquip elit dolore excepteur et. Laborum exercitation cupidatat culpa ut aute voluptate amet excepteur exercitation commodo mollit. Aliqua ipsum consectetur deserunt consectetur in Lorem anim adipisicing eiusmod eu laborum cillum ea. Est quis veniam minim id commodo fugiat veniam aliqua cupidatat aliquip minim duis.
                 </p>
